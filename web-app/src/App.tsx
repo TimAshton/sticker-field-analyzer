858a1ae5b094813029c4618aae5e32025a7f9f4b
Tim Ashton <tim.ashton.la@gmail.com>
2026-08-13 11:04:44 -0700

    chore: remove home btn and redirect / to the sticker book

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
 import Upload from './pages/Upload'
 import StickerBook from './pages/StickerBook'
 
@@ -7,13 +7,12 @@ function App() {
     <BrowserRouter>
       <div>
         <nav style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
-          <Link to="/">Home</Link>
           <Link to="/upload">Upload</Link>
           <Link to="/sticker-book">Sticker Book</Link>
         </nav>
 
         <Routes>
-          <Route path="/" element={<div>Sticker Field App</div>} />
+          <Route path="/" element={<Navigate to="/sticker-book" replace />} />
           <Route path="/upload" element={<Upload />} />
           <Route path="/sticker-book" element={<StickerBook />} />
         </Routes>
